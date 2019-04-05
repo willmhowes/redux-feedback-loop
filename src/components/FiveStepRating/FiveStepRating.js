b9e05fa139b033e230a63a64332628b98250ef60
Will Howes <willmhowes@gmail.com>
2019-04-05 17:33:30 -0500

    make so radio buttons save selected value to state

--- src/components/FiveStepRating/FiveStepRating.js
+++ src/components/FiveStepRating/FiveStepRating.js
@@ -1,22 +1,56 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import './FiveStepRating.css';
 
 class FiveStepRating extends Component {
+
+   state = {
+      selectedOption: null,
+   }
+
+   handleSubmit = (event) => {
+      event.preventDefault();
+
+   }
+
+   handleRadioUpdate = (event) => {
+      this.setState({
+         selectedOption: event.target.value,
+      })
+   }
+
    render() {
       console.log('in viewunderstanding');
       return (
-         <div>
-            <input type="radio" name="oneToFive" value="1"></input>
-            <label>1</label>
-            <input type="radio" name="oneToFive" value="2"></input>
-            <label>2</label>
-            <input type="radio" name="oneToFive" value="3"></input>
-            <label>3</label>
-            <input type="radio" name="oneToFive" value="4"></input>
-            <label>4</label>
-            <input type="radio" name="oneToFive" value="5"></input>
-            <label>5</label>
-         </div>
+         <form className="fiveStepRating-radioForm" onSubmit={this.handleSubmit}>
+            {JSON.stringify(this.state.selectedOption)}
+            <div className="fiveStepRating-radio" onClick={this.handleRadioUpdate}>
+               <input type="radio" name="oneToFive" value="1"></input>
+               <br />
+               <label>1</label>
+            </div>
+            <div className="fiveStepRating-radio" onClick={this.handleRadioUpdate}>
+               <input type="radio" name="oneToFive" value="2"></input>
+               <br />
+               <label>2</label>
+            </div>
+            <div className="fiveStepRating-radio" onClick={this.handleRadioUpdate}>
+               <input type="radio" name="oneToFive" value="3"></input>
+               <br />
+               <label>3</label>
+            </div>
+            <div className="fiveStepRating-radio" onClick={this.handleRadioUpdate}>
+               <input type="radio" name="oneToFive" value="4"></input>
+               <br />
+               <label>4</label>
+            </div>
+            <div className="fiveStepRating-radio" onClick={this.handleRadioUpdate}>
+               <input type="radio" name="oneToFive" value="5"></input>
+               <br />
+               <label>5</label>
+            </div>
+            <button type="submit">Submit</button>
+         </form>
       );
    }
 }
