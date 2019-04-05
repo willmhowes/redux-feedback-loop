ec7be5ed0cae4f35cc6a6be27a4f12a2b219914a
Will Howes <willmhowes@gmail.com>
2019-04-05 18:29:02 -0500

    make so that feedback is visibile while in progress of completion

--- src/components/FeedbackInProgress/FeedbackInProgress.js
+++ src/components/FeedbackInProgress/FeedbackInProgress.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import { connect } from 'react-redux';
+import './FeedbackInProgress.css';
 
 class FeedbackInProgress extends Component {
    render() {
       return (
-         <section className="FeedbackInProgress-section">
-            <h2>This is where the feedback in progress will display</h2>
+         <section>
+            <table className="FeedbackInProgress-table">
+               <thead>
+                  <tr>
+                     <th>Category</th>
+                     <th>Response</th>
+                  </tr>
+               </thead>
+
+               <tbody>
+                  <tr>
+                     <td>Feeling</td>
+                     <td>{this.props.reduxState.feedbackFormReducer.feelingRating}</td>
+                  </tr>
+                  <tr>
+                     <td>Understanding</td>
+                     <td>{this.props.reduxState.feedbackFormReducer.understandingRating}</td>
+                  </tr>
+                  <tr>
+                     <td>Support</td>
+                     <td>{this.props.reduxState.feedbackFormReducer.supportRating}</td>
+                  </tr>
+                  <tr>
+                     <td>comment</td>
+                     <td>{this.props.reduxState.feedbackFormReducer.comment}</td>
+                  </tr>
+               </tbody>
+            </table>
          </section>
       );
    }
