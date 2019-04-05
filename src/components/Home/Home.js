d00a354f534b81a3caaa1a62e444974340e2c6a9
Will Howes <willmhowes@gmail.com>
2019-04-05 16:40:55 -0500

    link home to viewFeeling

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
+import './Home.css';
 
 class Home extends Component {
+
+   linkToViewFeeling = () => {
+      this.props.history.push('/feeling');
+   }
+
    render() {
       return (
-         <div>
-
+         <div className="center">
+            <button onClick={this.linkToViewFeeling}>Begin Feedback</button>
          </div>
       );
    }
