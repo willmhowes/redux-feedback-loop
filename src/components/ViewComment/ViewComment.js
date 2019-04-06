4e85f7f547b188cfe5f61bf5534380e210ac7e91
Will Howes <willmhowes@gmail.com>
2019-04-06 15:24:18 -0500

    add text area

--- src/components/ViewComment/ViewComment.js
+++ src/components/ViewComment/ViewComment.js
@@ -3,10 +3,21 @@ import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 
 class ViewComment extends Component {
+
+   state = {
+      comment: null,
+   }
+
    render() {
       return (
          <div>
+            <h1>Any comments you want to leave?</h1>
 
+
+            <form onSubmit={}>
+               <textarea cols="60" rows="10"></textarea>
+               <button>Submit</button>
+            </form>
          </div>
       );
    }
