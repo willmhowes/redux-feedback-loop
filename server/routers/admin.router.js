9d0e42207ea1f74cbecb4e939ecaa6ad4ee88428
Will Howes <willmhowes@gmail.com>
2019-04-07 16:39:36 -0500

    add router for administrative GET and DELETE

--- server/routers/admin.router.js
+++ server/routers/admin.router.js
@@ -3,12 +3,28 @@ const router = express.Router();
 const pool = require('../modules/pool');
 
 // GET Route
-router.get('/admin', (req, res) => {
-
+router.get('/', (req, res) => {
+   pool.query(`SELECT * FROM "feedback" ORDER BY "id" DESC`)
+      .then((response) => {
+         res.send(response.rows);
+      }).catch((error) => {
+         console.log(`Error getting feedback entries:`, error);
+         res.sendStatus(500);
+      })
 }); // END GET Route
 
-router.delete('/admin/delete/:id', (req, res) => {
-   
+// DELETE ROUTE
+router.delete('/delete/:id', (req, res) => {
+   const id = req.params.id;
+   const sqlText = `DELETE FROM "feedback" WHERE "id" = $1;`;
+
+   pool.query(sqlText, [id])
+      .then(() => {
+         res.sendStatus(200);
+      }).catch((error) => {
+         console.log(`Error deleting feedback entry:`, error);
+         res.sendStatus(500);
+      })
 }); // END PUT Route
 
 module.exports = router;
